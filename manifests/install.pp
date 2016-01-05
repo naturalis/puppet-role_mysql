51c8fee60db7777e29b7b43d9d4b7792c04efa89
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-01-05 16:48:35 +0100

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,7 +5,6 @@ class role_mysql::install {
   # Install MySQL server
   class { 'mysql::server':
     root_password           => $role_mysql::root_password,
-    package_ensure          => $role_mysql::package_ensure,
     remove_default_accounts => $role_mysql::remove_default_accounts,
   } 
 
