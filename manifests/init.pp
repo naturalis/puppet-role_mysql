1c95dc708f9b2165c0997961e04845f61f5f71a8
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-01-07 11:11:30 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,7 +37,7 @@
 #
 class role_mysql (
   $mysql_root_password     = 'rootpassword',
-  $remove_default_accounts = true,
+  $remove_default_accounts = undef,
   $override_options        = undef,
   ) {
 
