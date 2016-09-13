0400835a6f974e3f4197f009c18cbf0b861553cb
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-09-13 14:33:18 +0200

    Add package version parameter package_ensure

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,8 +6,8 @@ class role_mysql::install {
   class { 'mysql::server':
     root_password           => $role_mysql::mysql_root_password,
     override_options        => $role_mysql::override_options,
+    package_ensure          => $role_mysql::package_ensure,
     remove_default_accounts => $role_mysql::remove_default_accounts,
-    #users                   => $role_mysql::users,
     grants                  => $role_mysql::grants
   } 
 
