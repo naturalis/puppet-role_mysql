02c0e6b00f0db45bdca0bfe0e1748bcb583af3c0
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2019-07-09 20:53:46 +0200

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,7 +5,7 @@ class role_mysql::install {
   # Install MySQL server
   class { 'mysql::server':
     root_password           => $role_mysql::mysql_root_password,
-    override_options        => $role_mysql::override_hash,
+    override_options        => parseyaml($role_mysql::override_options,$role_mysql::override_options),
     remove_default_accounts => $role_mysql::remove_default_accounts,
     package_name            => $role_mysql::package_name_server
   }
