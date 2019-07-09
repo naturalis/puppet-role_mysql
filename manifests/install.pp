0f4938f41b672f5474099f52482c6f1698e6d8ba
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2019-07-09 20:22:52 +0200

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,13 +7,7 @@ class role_mysql::install {
     root_password           => $role_mysql::mysql_root_password,
     override_options        => $role_mysql::override_hash,
     remove_default_accounts => $role_mysql::remove_default_accounts,
-    grants                  => $role_mysql::grants,
     package_name            => $role_mysql::package_name_server
   }
-
-  # Install MySQL client
-  # class { 'mysql::client':
-  #   package_name => $role_mysql::package_name_client
-  # }
 
 }
