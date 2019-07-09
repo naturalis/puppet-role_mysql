12068629671f0dd4158c7458942ed93f3a0f3224
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2019-07-09 21:16:37 +0200

    Update users.pp

--- manifests/users.pp
+++ manifests/users.pp
@@ -3,7 +3,7 @@
 class role_mysql::users {
 
   if ($role_mysql::users) {
-    create_resources('role_mysql::defines::users', $role_mysql::users)
+    create_resources('role_mysql::defines::users', parseyaml($role_mysql::users,$role_mysql::users))
   }
   
 }
