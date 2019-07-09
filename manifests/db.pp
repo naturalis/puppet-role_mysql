ffa4c4d45d70fd3deca30f4aebac04e9a1a872bc
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2019-07-09 20:06:01 +0200

    Update db.pp

--- manifests/db.pp
+++ manifests/db.pp
@@ -3,7 +3,7 @@
 class role_mysql::db {
 
   if ($role_mysql::db_hash) {
-    create_resources('mysql::db', $role_mysql::db_hash)
+    create_resources(mysql::db, parseyaml($role_mysql::db_hash,$role_mysql::db_hash))
   }
   
 }
